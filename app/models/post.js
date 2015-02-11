0996df3eac1d23a6a3b297b2a4cd10f6ffcbab25
Kendra <kdodds@gmail.com>
2015-02-11 13:53:02 -0700

    working through associations

--- app/models/post.js
+++ app/models/post.js
@@ -5,4 +5,6 @@ import DS from "ember-data";
 export default DS.Model.extend({
   body: DS.attr('string'),
   title: DS.attr('string')
+  // user: DS.belongsTo('user')
+  // author: DS.belongsTo('user')
 });
